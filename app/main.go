ec57bf424be8b3e005f9e01f2eb86ee6f0f76dd3
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2025-10-02 16:13:29 +0200

    try #34

--- app/main.go
+++ app/main.go
@@ -342,7 +342,7 @@ func handleConnection(conn net.Conn) {
 
 	responseMsg := &KafkaMessage{
 		Header: responseHeader,
-		Body:   &ApiVersionsResponseV4{ErrorCode: 123, ApiKeys: []ApiVersion{{ApiKey: 18, MinVersion: 0, MaxVersion: 4}}, ThrottleTimeMs: 0},
+		Body:   &ApiVersionsResponseV4{ErrorCode: 000000, ApiKeys: []ApiVersion{{ApiKey: 18, MinVersion: 0, MaxVersion: 4}}, ThrottleTimeMs: 0},
 	}
 
 	// conn.Read(make([]byte, 1024)) // Leggo il resto del messaggio (che non mi interessa)
